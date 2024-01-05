88e681bda3712efdca036e4b3822c5ee7d6421b9
Moreen <moreencharles@gmail.com>
2024-01-05 19:21:24 +0300

    Completed Assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            raise ValueError("The value must be a string.\n")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        replaced_value = self.value.replace('.', '@').replace('?', '@').replace('!', '@')
+        sentences = replaced_value.split('@')
+        sentences = [sentence for sentence in sentences if sentence]
+        return len(sentences)
